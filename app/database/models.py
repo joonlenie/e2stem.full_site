1c971eb541d17332f89f900bd6b877bc3b098161
thizzbuzz <mf88mf88@gmail.com>
2022-03-29 16:03:59 +0700

    ...

--- app/database/models.py
+++ app/database/models.py
@@ -21,3 +21,22 @@ class Student(models.Model):
     class Meta:
 
        ordering = ['created_at']
+
+# Blog model
+
+class BlogPost(models.Model):
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    text = models.TextField()    
+    
+    created_at = models.DateField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+    
+    def __str__(self):
+
+        return self.name
+
+    class Meta:
+
+       ordering = ['created_at']
